06b85ebe92a41405add08583a572b169fc41ee3e
Rohan <rohangorla07@gmail.com>
2024-05-15 01:01:09 +0530

    add path to views folder

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
-app.set('views', __dirname + '/../views');
+app.set('views', __dirname + '/views');
 
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  // res.render('index');
-  res.send("server is working");
+  res.render('index');
+  // res.send("server is working");
 });
 
 app.get('/about', (req, res)=>{
-  // res.render('index', {person: 'Rohan'});
-  res.send("about is working");
+  res.render('index', {person: 'Rohan'});
+  // res.send("about is working");
 })
 
 app.listen(PORT, () => {
